e0d286b0acbe178783904d240fad257824523f06
syed.abbas00@gmail.com <syed.abbas00@gmail.com>
2026-01-21 15:54:57 +0400

    updated package.json and yml file

--- output/claims/process-claims.bat
+++ output/claims/process-claims.bat
@@ -1,11 +1,11 @@
 @echo off
 echo Processing XML files...
-echo Processing AL_1768988561902_2.xml
-echo Processing AL_1768988561962_2.xml
-echo Processing MO_1768988561901_1.xml
-echo Processing MO_1768988561960_1.xml
-echo Processing TN_1768988561898_0.xml
-echo Processing TN_1768988561959_0.xml
+echo Processing AL_1768996372483_2.xml
+echo Processing AL_1768996372525_2.xml
+echo Processing MO_1768996372480_1.xml
+echo Processing MO_1768996372523_1.xml
+echo Processing TN_1768996372476_0.xml
+echo Processing TN_1768996372521_0.xml
 
 echo All files processed.
 pause
