e300fbebf28c8aeb3e9b796a3e41510e794bf181
syed.abbas00@gmail.com <syed.abbas00@gmail.com>
2026-01-30 10:23:55 +0400

    updated changes in the code

--- output/claims/process-claims.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Processing XML files...
-echo Processing MKMO01222026-14168.xml
-echo Processing MKMO01222026-14181.xml
-echo Processing MKMO01222026-14184.xml
-
-echo All files processed.
-pause
